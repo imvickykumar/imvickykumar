de6bb06162db4fbf90529a391eeda0dad79c32c3
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-23 17:02:05 +0530

    Create realtimeplot.py

---
+++ Graph Sensor Data/realtimeplot.py
@@ -0,0 +1,44 @@
+
+# https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/update-a-graph-in-real-time#:~:text=To%20create%20a%20real%2Dtime,new%20frame%20in%20the%20animation.
+
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import tmp102
+
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+# Initialize communication with TMP102
+tmp102.init()
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    # Read temperature (Celsius) from TMP102
+    temp_c = round(tmp102.read_temp(), 2)
+
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(temp_c)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
